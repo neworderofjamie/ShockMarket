1503ab4c59456009edadd0aa9edc5afd61ad80ce
Stefan Meyer <sm961@sussex.ac.uk>
2021-02-09 19:33:14 +0100

    Added transition tendencies

--- Market.cpp
+++ Market.cpp
@@ -4,21 +4,57 @@
 
 GrowthModel::GrowthModel()
 {
-    state = "G";
+    // init hidden model params
+    modelName = "Markov";
     transitionTendency = 0;
-    distributionParams g = {0.f, 1.f, "Normal"};
-    emissionProperty.insert(std::pair<string,distributionParams>("G",g));  
-}
-
-GrowthModel::GrowthModel(string startState)
-{
-    state = startState;
+    distributionParams g = {.5f, 1.f, "Normal"};
+    distributionParams s = {0.f, 1.f, "Normal"};
+    distributionParams f = {-.5f, 1.f, "Normal"};
     
+    // store emission distribution parameters for this instance
+    emissionProperty.insert(std::pair<string,distributionParams>("G",g));
+    emissionProperty.insert(std::pair<string,distributionParams>("S",s));
+    emissionProperty.insert(std::pair<string,distributionParams>("F",f));  
+    
+    // set transition between states
+    // [-1:-0.34] -> tendency towards "falling" state
+        // [-0.33:0.33] -> tendency towards "stagnating" state
+        // [0.34:1] -> tendeny towards "rising" state
+    thFalling2Stagnation = -.34f;
+    thStagnation2Growing = .33f;
+    
+    // set starting state
+    state = "S";
+
 }
 
-string GrowthModel::getState()
+GrowthModel::GrowthModel(string startState,
+                    string hiddenModelName,
+                    float initialTransitionTendency,
+                    string initialState,
+                    distributionParams f,
+                    distributionParams s,
+                    distributionParams g,
+                    float thF2S,
+                    float thS2G)
 {
-    return this->state;
+    // init hidden model params
+    modelName = hiddenModelName;
+    transitionTendency = initialTransitionTendency;
+    // store emission distribution parameters for this instance
+    emissionProperty.insert(std::pair<string,distributionParams>("G",g));
+    emissionProperty.insert(std::pair<string,distributionParams>("S",s));
+    emissionProperty.insert(std::pair<string,distributionParams>("F",f));  
+    
+    // set transition between states
+    thFalling2Stagnation = thF2S;
+    thStagnation2Growing = thS2G;
+    
+    // set starting state
+    state = initialState;
+
+
+    
 }
 
 distributionParams GrowthModel::getEmissionProperty(string state)
@@ -27,4 +63,21 @@ distributionParams GrowthModel::getEmissionProperty(string state)
     return this->emissionProperty[state];
 }
         
+void GrowthModel::update(marketEvent event)
+{
+    this->transitionTendency = this->transitionTendency+event.influence; 
+}
 
+/*
+GETTER/SETTER
+*/
+
+string GrowthModel::getState()
+{
+    return this->state;
+}
+
+float GrowthModel::getTransitionTendency()
+{
+    return this->transitionTendency
+}
--- Market.h
+++ Market.h
@@ -14,24 +14,52 @@ struct distributionParams
     string distName;
 };
 
+struct marketEvent 
+{
+    string name;
+    float influence;
+};
+
 class GrowthModel
 {
     public:
+        // constructors, TODO add constructor that loads from JSON file
         GrowthModel();
-        GrowthModel(string startState);
-        GrowthModel(string startState, std::map<string,distributionParams> emissionProperty);
+        GrowthModel(string startState,
+                    string hiddenModelName,
+                    float initialTransitionTendency,
+                    string initialState,
+                    distributionParams f,
+                    distributionParams s,
+                    distributionParams g,
+                    float thF2S,
+                    float thS2G);
+        // getter
         string getState();
+        float getTransitionTendency();
+        float getGrowthRate();
         distributionParams getEmissionProperty(string state);
-        float getEmission();
+        float getGrowthRate();
+        
+        // update function to use to feed events into the growth model and advance it
+        void update();
+        void update(marketEvent event);
+
     private:
         // identifier for the state the model is in "G", "S" or "F"
         string state;
+        // name of the underlyinng model
+        string modelName;
         // Map mapping the State to the relevant distribution info
         std::map<string,distributionParams> emissionProperty; 
         // value between zero and [-1,1] inidcating the tendency to change states
         // [-1:-0.34] -> tendency towards "falling" state
         // [-0.33:0.33] -> tendency towards "stagnating" state
         // [0.34:1] -> tendeny towards "rising" state
+        float thFalling2Stagnation;
+        float thStagnation2Growing;
+        
+        
         float transitionTendency;
     
 };
